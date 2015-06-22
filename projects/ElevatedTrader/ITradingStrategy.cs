2b7819a1b007d9fab5848bb70231e148ec01f8eb
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2015-06-22 09:40:50 -0700

    added session and strategy interfaces

---
+++ projects/ElevatedTrader/ITradingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatedTrader
+{
+	public interface ITradingStrategy
+	{
+		void Tick(ITradePeriod period, bool finalize = false);
+	}
+}

cbdb5c4894a3bd15a10fe2e5bedd6bec019a8e29
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2015-06-23 07:33:32 -0700

    added indicator settings interface

---
+++ projects/ElevatedTrader/Indicators/IIndicatorSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatedTrader.Indicators
+{
+	public interface IIndicatorSettings
+	{
+		object Settings { get; set; }
+	}
+
+	public interface IIndicatorSettings<T> : IIndicatorSettings
+	{
+		new T Settings { get; set; }
+	}
+}

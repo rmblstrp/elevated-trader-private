bec5b8ad48a0f1bd2ffeb4656b198e4f73df4d42
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2015-08-06 21:33:56 -0700

    added interfaces to analyze simulated sessions

---
+++ projects/ElevatedTrader/IMultiSessionAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatedTrader
+{
+	public interface IMultiSessionAnalyzer
+	{
+		double AverageEquity { get; }
+
+		double AverageTotalGain { get; }
+
+		double AverageTotalLoss { get; }
+
+		double AverageHighestTradeGain { get; }
+
+		double AverageHighestTradeLoss { get; }
+
+		double AverageMaximumGain { get; }
+
+		double AverageMaximumLoss { get; }
+
+		double AverageLargestDrawdown { get; }
+
+		double AverageLongestDrawdownDuration { get; }
+
+		double AverageLargestStreak { get; }
+
+		double AverageLongestStreakDuration { get; }
+
+		double AverageTradeCount { get; }
+
+		// average $ winners * win % + average $ losers * lose %
+		// (average $ winners * win % + average $ losers * lose %) / -(average $ losers)
+		double AverageExpectancy { get; }
+
+		void Analyze(ISessionAnalyzer analyzer);
+	}
+}
---
+++ projects/ElevatedTrader/ISessionAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatedTrader
+{
+	public interface ISessionAnalyzer
+	{
+		double Equity { get; }
+
+		double TotalGain { get; }
+
+		double TotalLoss { get; }
+
+		double HighestTradeGain { get; }
+
+		double HighestTradeLoss { get; }
+
+		// Max value above 0
+		double MaximumGain { get; }
+
+		// Max value below 0
+		double MaximumLoss { get; }
+
+		double LargestDrawdown { get; }
+
+		int LongestDrawdownDuration { get; }
+
+		double LargestStreak { get; }
+
+		int LongestStreakDuration { get; }
+
+		int TradeCount { get; }
+
+		// average $ winners * win % + average $ losers * lose %
+		// (average $ winners * win % + average $ losers * lose %) / -(average $ losers)
+		double Expectancy { get; }
+
+		void Analyze(ITradingSession session);
+	}
+}
